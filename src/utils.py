9282886d937afa068034da51d639e36f7ab08184
hyperintellectual <usually.rabbit@gmail.com>
2025-09-26 17:15:15 +0300

    bug fix

--- src/utils.py
+++ src/utils.py
@@ -4,19 +4,27 @@ import torch
 import transformers
 from torch.utils.data import DataLoader
 from typing import Dict
+from src.logger import get_logger
+
+logger = get_logger()
 
 
+# Ensure deterministic behaviour
 def set_seed(seed):
     random.seed(seed)
     np.random.seed(seed)
     torch.manual_seed(seed)
     torch.cuda.manual_seed_all(seed)
     transformers.set_seed(seed)
+
     # Ensure deterministic behaviour on CUDA
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = False
+
     # Warn if non-deterministic algorithms are used
-    torch.use_deterministic_algorithms(True, warn_only=True)
+    torch.use_deterministic_algorithms(True, warn_only=True)  # warning won't be logged
+
+    logger.info(f"Seed is set to '{seed}'")
 
 
 def get_num_training_steps(dataloader: DataLoader, config_dict: Dict):
