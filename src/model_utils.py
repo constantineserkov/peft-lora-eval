2e9766383dbf16e645f1e9c724c08d0027767caf
hyperintellectual <usually.rabbit@gmail.com>
2025-09-27 17:03:15 +0300

    configure model_utils to check for the peft method

--- src/model_utils.py
+++ src/model_utils.py
@@ -2,9 +2,13 @@ from transformers import AutoModelForCausalLM
 import torch
 from peft import LoraConfig, get_peft_model, TaskType
 from typing import Dict
+from src.logger import get_logger
+
+logger = get_logger()
 
 
 def configure_peft_model(model_name: str, config_dict: Dict, device: str) -> torch.nn.Module:
+    logger.debug("configuring a model")
     model = AutoModelForCausalLM.from_pretrained(
         model_name,
         low_cpu_mem_usage=True,
@@ -12,14 +16,30 @@ def configure_peft_model(model_name: str, config_dict: Dict, device: str) -> tor
         offload_folder="offload"
     )
 
-    peft_config = LoraConfig(
-        task_type=TaskType.CAUSAL_LM,
-        inference_mode=False,
-        r=config_dict['r'],
-        lora_alpha=config_dict['lora_alpha'],
-        use_rslora=config_dict['use_rslora'],
-        target_modules=config_dict['target_modules']
-    )
+    if config_dict['method'] == "base":
+        logger.info("PEFT is not used.")
+    elif config_dict['method'] == "lora":
+        logger.info("PEFT method: LoRA")
+        peft_config = LoraConfig(
+            task_type=TaskType.CAUSAL_LM,
+            inference_mode=False,
+            r=config_dict['r'],
+            lora_alpha=config_dict['peft_config']['lora_alpha'],
+            use_rslora=config_dict['peft_config']['use_rslora'],
+            target_modules=config_dict['peft_config']['target_modules']
+        )
+        model = get_peft_model(model, peft_config)
+    elif config_dict['method'] == "qlora":
+        logger.info("PEFT method: QLoRA")
+        pass
+    elif config_dict['method'] == "dora":
+        logger.info("PEFT method: DoRA")
+        pass
+    elif config_dict['method'] == "qdora":
+        logger.info("PEFT method: QDoRA")
+        pass
+    else:
+        logger.error("There is no way that you see this error message")
+        raise ValueError
 
-    model = get_peft_model(model, peft_config)
     return model.to(device)
