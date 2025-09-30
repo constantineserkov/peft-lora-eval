3762c215ebfdd128ba9022e30aaee895b65db608
hyperintellectual <usually.rabbit@gmail.com>
2025-09-30 12:00:28 +0300

    set up compute_training_metrics

--- src/trainer.py
+++ src/trainer.py
@@ -1,6 +1,7 @@
 import logging
 import wandb
 import torch
+import numpy as np
 import transformers
 from torch.cuda.amp import autocast, GradScaler
 from torch.utils.data import DataLoader
@@ -9,6 +10,7 @@ from torch.optim import Optimizer
 from torch.optim.lr_scheduler import LambdaLR
 from src.logger import get_logger
 from bitsandbytes.optim import PagedAdamW8bit
+import pynvml
 
 logger = get_logger()
 
@@ -43,8 +45,27 @@ def setup_scheduler(optimizer: Optimizer, config_dict: Dict) -> LambdaLR:
     )
 
 
-def compute_training_metrics() -> Dict:
-    return {}
+def compute_training_metrics(avg_train_loss: float, avg_eval_loss: float, device_index: int) -> Dict[str, float]:
+    """
+    Computes the following metrics:
+        1. Avg_train_loss
+        2. Avg_eval_loss
+        3. Perplexity: (exp(eval_loss))
+        4. VRAM usage
+
+    Returns:
+        Dict[str, float]: A dictionary with keys:
+            - "train_loss": float
+            - "eval_loss": float
+            - "perplexity": float
+            - "vram_usage": float (in MB or GB)
+    """
+    return {
+        "train_loss": avg_train_loss,
+        "eval_loss": avg_eval_loss,
+        "perplexity": np.exp(avg_eval_loss),
+        "vram_usage": log_vram_usage(device_index)
+    }
 
 
 def train_model(
@@ -106,17 +127,21 @@ def train_model(
             optimizer.zero_grad()
 
         if eval_dataloader:
-            metrics = compute_training_metrics()
+            # create two val loaders one small and one bigger for every epoch and every 1k steps validation
+            metrics = compute_training_metrics(avg_train_loss, avg_eval_loss)
 
         epoch_loss = sum(train_losses) / len(train_losses)
         print(f"\n\nEpoch {epoch} average loss: {epoch_loss}\n\n")
 
         # release unused cached memory back to the GPU
         torch.cuda.empty_cache()
+        pynvml.nvmlShutdown()
 
 
-def log_vram_usage(device: Optional[str]) -> float:
-    pass
+def log_vram_usage(device_index: int = 0) -> float:
+    handle = pynvml.nvmlDeviceGetHandleByIndex(device_index)
+    mem_info = pynvml.nvmlDeviceGetMemoryInfo(handle)
+    return mem_info / (1024 ** 2)  # MB
 
 
 # save model's checkpoints to disk each N num_steps
