cce01941739e2fe4cab92775de272a18e3ec9548
hyperintellectual <usually.rabbit@gmail.com>
2025-11-18 07:57:25 +0300

    logger troubles

--- scripts/run_evaluate.py
+++ scripts/run_evaluate.py
@@ -13,9 +13,9 @@ def main():
     wc_start = time.time()
 
     # setup logging base config
-    set_up_logging()
+    console_handler, file_handler = set_up_logging()
 
-    logger = get_logger()
+    logger = get_logger(console_handler, file_handler)
 
     device = "cuda" if torch.cuda.is_available() else "cpu"
     logger.info(f"Device: {device}")
--- src/logger.py
+++ src/logger.py
@@ -8,12 +8,15 @@ import atexit
 # configure a single global handler
 def set_up_logging(
         log_filename: str = "results/logs/project.log"
-)-> None:
-    from src.utils import in_colab
-    if in_colab():
+):
+    from src.utils import in_colab, in_kaggle
+    local = True
+    if in_colab() or in_kaggle():
+        logging.debug("Working in colab")
         # remove existing handlers
         for handler in logging.root.handlers[:]:
             logging.root.removeHandler(handler)
+        local = False
 
     log_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", log_filename)
     os.makedirs(os.path.dirname(log_path), exist_ok=True)
@@ -30,14 +33,16 @@ def set_up_logging(
             }
     ))
 
-    file_handler = logging.FileHandler(log_filename)
+    file_handler = logging.FileHandler(log_path)
     file_handler.setFormatter(logging.Formatter(
         "%(asctime)s [%(levelname)s] %(name)s: %(message)s"
     ))
 
-    # root logger config
-    logging.basicConfig(level=logging.DEBUG, handlers=[console_handler, file_handler])
-
+    if local:
+        # root logger config
+        logging.basicConfig(level=logging.DEBUG, handlers=[console_handler, file_handler])
+        return console_handler, file_handler
+    return console_handler, file_handler
 
     # NVML init here (once per call, but since module-level, once per import)
     try:
@@ -48,9 +53,13 @@ def set_up_logging(
     except Exception as e:
         print(f"Failed to initialize NVML in logger: {e}. VRAM logging disabled.")
 
-def get_logger(name: str = __name__):
+def get_logger(console_handler, file_handler, name: str = __name__):
+
     # setLevel debug is temporary
     logger = logging.getLogger(name)
     logger.setLevel(logging.DEBUG)
+    logger.handlers = []
+    logger.addHandler(console_handler)
+    logger.addHandler(file_handler)
     return logger
 
